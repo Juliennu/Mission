180cd5d11f97d4e963f6b166b9740232e966d6d6
Juliennu <wsb.1099@gmail.com>
2021-07-09 18:52:17 +0900

    フォルダータブのTableViewを設定

--- Mission/ViewControllers/FolderViewController.swift
+++ Mission/ViewControllers/FolderViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class FolderViewController: UIViewController {
 
+    @IBOutlet weak var folderListTableView: UITableView!
+    @IBOutlet weak var folderListSearchBar: UISearchBar!
+    
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var addNewFolderButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        folderListTableView.delegate = self
+        folderListTableView.dataSource = self
+        
+        folderListTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
     }
     
 
+}
+
+extension FolderViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = folderListTableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.textLabel?.text = "死ぬまでにやりたいこと"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        folderListTableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
 }
